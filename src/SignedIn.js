dd1cd7b37a8439ea3d7265c7cca992ebd6a67f6d
Will Schulz <will.schulz29@gmail.com>
2019-05-06 12:10:36 -0400

    added Jawad's splash page and different register routing

--- src/SignedIn.js
+++ src/SignedIn.js
@@ -1,8 +1,6 @@
-import React, {useState} from 'react'
-import Navigation from './components/Navigation.js';
-import {Router, Route} from 'react-router-dom';
 
-
+import React from 'react'
+import firebase from 'firebase'
 
 
 const Signedin = () => {
@@ -10,10 +8,7 @@ const Signedin = () => {
 
     return (
         <div>
-            <Navigation />
-            {/* <Router>
-                <Route />
-            </Router> */}
+            <button onClick={() => firebase.auth().signOut()}> Sign Out </button>
         </div>
     )
 }
