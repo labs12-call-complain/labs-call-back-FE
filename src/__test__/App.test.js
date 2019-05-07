d1f3dcc679f70cce8e50b27eca2912602f0a00c3
tactnician <tactnician@gmail.com>
2019-05-06 20:00:23 -0400

    added test folder - moved imgs to public folder

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
+
+import {render} from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders sucessfully', () => {
+  render(<App />);
+})
