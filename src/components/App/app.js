32b366e4f5ce08317c854a5192ea6eeacbfbc6e5
jmendez44 <jenuel.mendez@gmail.com>
2019-05-15 18:51:35 -0400

    debug

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -10,7 +10,7 @@ import HomePage from '../Home/home.js';
 import AccountPage from '../Account/account.js';
 import AdminPage from '../Admin/admin.js';
 import FormContainer from '../Forms/FormContainer';
-import Profile from '../Users/Profile'
+import Profile from '../Users/Profile.js'
 import TweetForm from '../Forms/TweetForm.js'
 
 import * as ROUTES from '../constants/routes.js';
