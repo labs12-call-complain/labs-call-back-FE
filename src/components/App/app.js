8cdf952be52f80df183a4558e45b5fa73c14d8c1
Will Schulz <will.schulz29@gmail.com>
2019-05-22 14:47:17 -0400

    trying lump changes

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -3,16 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation/navigation.js';
 import LandingPage from '../Landing/landing.js';
-import SignUpPage from '../SignUp/signup.js';
-import SignInPage from '../SignIn/signin.js';
-import PasswordForgetPage from '../PasswordForget/passwordforget.js';
 import HomePage from '../Home/home.js';
-import AccountPage from '../Account/account.js';
-import AdminPage from '../Admin/admin.js';
 import FormContainer from '../Forms/FormContainer';
 import EditProfile from '../Users/EditProfile.js';
 import Profile from '../Users/Profile.js'
 
+import './App.css'
 
 import * as ROUTES from '../constants/routes.js';
 
@@ -20,18 +16,15 @@ import { withAuthentication } from '../Session/session.js';
 
 const App = () => (
     <Router>
-    <div>
-    
-        
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.EDIT_PROFILE} component={EditProfile} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.FORM_CONTAINER} component={FormContainer} />
-        <Route path={ROUTES.PROFILE} component={Profile} />
-    </div>
+        <div className="App">
+            <Navigation />
+            
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.EDIT_PROFILE} component={EditProfile} />
+            <Route path={ROUTES.HOME} component={HomePage} />
+            <Route path={ROUTES.FORM_CONTAINER} component={FormContainer} />
+            <Route path={ROUTES.PROFILE} component={Profile} />
+        </div>
     </Router>
 );
 
