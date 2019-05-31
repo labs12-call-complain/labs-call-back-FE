a9777f9430e3a77033de516c18d0aeb7fc2f54ea
jmendez44 <jenuel.mendez@gmail.com>
2019-05-31 02:55:24 -0400

    -

--- src/__test__/Forms.test.js
+++ src/__test__/Forms.test.js
@@ -1,12 +1,12 @@
 import React from "react";
-import test from "react-test-renderer";
+import {render, fireEvent, cleanup} from '@testing-library/react';
 
 import Form1 from "../components/Forms/Form1"
 import Form2 from "../components/Forms/Form2"
 
 describe("<Form1 />", () => {
   it("matches snapshot", () => {
-    const tree = test.create(<Form1 />);
+    const tree = render.create(<Form1 />);
 
     expect(tree.toJSON()).toMatchSnapshot();
   });
@@ -14,7 +14,7 @@ describe("<Form1 />", () => {
 
 describe("<Form2 />", () => {
   it("matches snapshot", () => {
-    const tree = test.create(<Form2 />);
+    const tree = render.create(<Form2 />);
 
     expect(tree.toJSON()).toMatchSnapshot();
   });
